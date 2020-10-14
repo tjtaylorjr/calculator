1395faedde91712d66d14d3e1b2fb1940390e9ee
TJ Taylor <tjtaylorjr@gmail.com>
2020-10-14 14:01:02 -0400

    completed phase 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Calculator from './Calculator';
 
-function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+const App = () => (
+  <>
+    <h1>Calculator</h1>
+    <Calculator />
+  </>
+);
 
 export default App;
---
+++ src/Calculator.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+      num1: '',
+      num2: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Time for math!</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
