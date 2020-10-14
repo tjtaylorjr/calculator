a69940717b100a7fa48ef4a246fefe05256a85b4
Beau Kenny <beau.kenny@yahoo.com>
2020-10-14 13:23:33 -0500

    phase 4 complete

--- src/Calculator.js
+++ src/Calculator.js
@@ -10,10 +10,22 @@ class Calculator extends React.Component {
     };
   }
 
+  handleFirstNum = e => {
+    const num1 = Number(parseInt(e.target.value));
+    this.setState({num1});
+  };
+
+  handleSecondNum = e => {
+    const num2 = Number(parseInt(e.target.value));
+    this.setState({num2});
+  };
+
   render() {
     return (
       <div>
-        <h1>Time for math!</h1>
+        <h1>Result: {this.state.result}</h1>
+        <input value={this.state.num1} onChange={this.handleFirstNum} placeholder="First Number"/>
+        <input value={this.state.num2} onChange={this.handleSecondNum} placeholder="Second Number"/>
       </div>
     );
   }
