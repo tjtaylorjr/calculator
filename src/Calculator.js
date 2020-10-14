24c24f2be7a6241d87a1e80bb8f3c8e5a17c2d31
TJ Taylor <tjtaylorjr@gmail.com>
2020-10-14 15:48:05 -0400

    Completed simple calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -20,12 +20,50 @@ class Calculator extends React.Component {
     this.setState({num2});
   };
 
+  add = () => {
+    this.setState({
+      result: this.state.num1 + this.state.num2
+    })
+  }
+
+  subtract = () => {
+    this.setState({
+      result: this.state.num1 - this.state.num2
+    })
+  }
+
+  multiply = () => {
+    this.setState({
+      result: this.state.num1 * this.state.num2
+    })
+  }
+
+  divide = () => {
+    this.setState({
+      result: this.state.num1 / this.state.num2
+    })
+  }
+
+  clear = () => {
+    this.setState({
+      result: 0,
+      num1: '',
+      num2: ''
+    })
+  }
+
   render() {
+    const {result, num1, num2} = this.state;
     return (
       <div>
-        <h1>Result: {this.state.result}</h1>
-        <input value={this.state.num1} onChange={this.handleFirstNum} placeholder="First Number"/>
-        <input value={this.state.num2} onChange={this.handleSecondNum} placeholder="Second Number"/>
+        <h1>Result: {result}</h1>
+        <input value={num1} onChange={this.handleFirstNum} placeholder="First Number"/>
+        <input value={num2} onChange={this.handleSecondNum} placeholder="Second Number"/>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.divide}>/</button>
+        <button onClick={this.clear}>C</button>
       </div>
     );
   }
